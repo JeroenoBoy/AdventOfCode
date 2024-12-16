9e51e2d3064cca6e36ef7153a39fc78285ca5b4a
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-12-16 09:54:37 +0100

    Make functions public

--- utils/array2d.go
+++ utils/array2d.go
@@ -6,7 +6,7 @@ type Array2D[T any] struct {
 	Height int
 }
 
-func newArray2D[T any](width, height int) Array2D[T] {
+func NewArray2D[T any](width, height int) Array2D[T] {
 	return Array2D[T]{
 		array:  make([]T, width*height),
 		Width:  width,
@@ -14,14 +14,14 @@ func newArray2D[T any](width, height int) Array2D[T] {
 	}
 }
 
-func (arr *Array2D[T]) contains(x, y int) bool {
+func (arr *Array2D[T]) Contains(x, y int) bool {
 	return x >= 0 && x < arr.Width && y >= 0 && y < arr.Height
 }
 
-func (arr *Array2D[T]) get(x, y int) T {
+func (arr *Array2D[T]) Get(x, y int) T {
 	return arr.array[y*arr.Width+x]
 }
 
-func (arr *Array2D[T]) set(x, y int, nv T) {
+func (arr *Array2D[T]) Set(x, y int, nv T) {
 	arr.array[y*arr.Width+x] = nv
 }
